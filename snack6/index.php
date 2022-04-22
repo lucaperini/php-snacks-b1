25dbac8a5aeeea10bc3c97efdb5711ba216a9ada
lucaperini <luca.perini@gmail.com>
2022-04-22 16:01:54 +0200

    correzione snack 6

--- snack6/index.php
+++ snack6/index.php
@@ -30,15 +30,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Php-Snack-6</title>
 </head>
 <body>
-
-<div class="grey">
-    
-</div>
-<div class="green"></div>
-
-    </body>
+<?php
+    $keys = array_keys($db);
+    $values = array_values($db);
+    for ($index=0; $index < count($values); $index++) { 
+        if ($keys[$index] == 'teachers') {
+            echo '<div class="grey">';
+            echo '<h5>Teachers</h5>';
+            for ($teacherIndex=0; $teacherIndex < count($values[$index]); $teacherIndex++) { 
+                $teacher = $values[$index][$teacherIndex];
+                echo $teacher['name']. ' ' . $teacher['lastname'] . '<br>';
+            }
+            echo '</div>';
+        }
+        else if ($keys[$index] == 'pm') {
+            echo '<div class="green">';
+            echo '<h5>PM</h5>';
+            for ($pmIndex=0; $pmIndex < count($values[$index]); $pmIndex++) { 
+                $pm = $values[$index][$pmIndex];
+                echo $pm['name']. ' ' . $pm['lastname'] . '<br>';
+            }
+        }
+        echo '</div>';
+    }
+?>
+</body>
 </html>
 
